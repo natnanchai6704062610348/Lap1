68f8beeff76ec2300e7e08a9f4402e49ddd1c14d
natnanchai6704062610348 <s6704062610348@email.kmutnb.ac.th>
2024-11-26 15:05:36 +0700

    ข้อที่1

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-int main() {
-	int var1;
-	int var2;
-	scanf("%d",&var1);
-	scanf("%d",&var2);
-	if (var1 > var2) {
-		printf("var1 is greater than var2");
-	}else{
-		printf("var2 is greater than var1");
-	}
+#include <stdio.h>
+int main(){
+    int MyNum1 = 15;
+    int MyNum2 = 10;
+    
+    if (MyNum1 > MyNum2){
+    	printf("MyNum1 is greater than MyNum2");
+    }
+    else{
+    	printf("MyNum2 is greater than MyNum1");
+    }
 }
