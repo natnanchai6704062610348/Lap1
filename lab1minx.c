af5d310df06d2cdd3115df37372f168250f272f9
natnanchai6704062610348 <s6704062610348@email.kmutnb.ac.th>
2024-12-02 00:09:57 +0700

    Add files via upload

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-int main(){
-    int n1,n2;
-    scanf("%d",&n1);
-    scanf("%d",&n2);
-    if (n1 > n2){
-        printf("n1 is more n2");
-    }else{
-        printf("n2 s more n1");
-    }
-    
+#include<stdio.h>
+int main() {
+	int var1;
+	int var2;
+	scanf("%d",&var1);
+	scanf("%d",&var2);
+	if (var1 > var2) {
+		printf("var1 is greater than var2");
+	}else{
+		printf("var2 is greater than var1");
+	}
 }
