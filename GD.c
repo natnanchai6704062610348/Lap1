b2d6db75bd833f203dcaafe8bcd907aa8b78720e
natnanchai6704062610348 <s6704062610348@email.kmutnb.ac.th>
2025-12-08 11:34:48 +0700

    Add files via upload

---
+++ GD.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	
+	switch(n){
+		case 1: printf("One\n"); break;
+		case 2: printf("Two\n"); break;
+		case 3: printf("Three\n"); break;
+		case 4: printf("Four\n"); break;
+		case 5: printf("Five\n"); break;
+		case 6: printf("Six\n"); break;
+		case 7: printf("Seven\n"); break;
+		case 8: printf("Eing\n"); break;
+		case 9: printf("Nice\n"); break;
+		
+		default:
+			printf("error");
+	}
+	
+	
+}
